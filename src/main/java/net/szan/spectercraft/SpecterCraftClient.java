d49940822fa41b379a223393d511d5d9273ae677
root <root@OMEN-SZ.localdomain>
2025-07-14 23:47:19 +0200

    Initial commit

---
+++ src/main/java/net/szan/spectercraft/SpecterCraftClient.java
@@ -0,0 +1,10 @@
+package net.szan.spectercraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpecterCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/szan/spectercraft/SpecterCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.szan.spectercraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpecterCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
