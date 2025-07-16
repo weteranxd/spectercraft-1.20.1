5dac6e9a7226845fa75f6730a41b5d7735a1c25a
Szan <weteran.xd250@gmail.com>
2025-07-16 20:43:54 +0200

    zmiany

---
+++ src/main/java/net/szan/spectercraft/block/ModBlocks.java
@@ -0,0 +1,35 @@
+package net.szan.spectercraft.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.szan.spectercraft.SpecterCraft;
+
+public class ModBlocks {
+
+    public static final Block ACID = registerBlock("acid",
+            new Block(FabricBlockSettings.copyOf(Blocks.HONEY_BLOCK).mapColor(MapColor.LIME)));
+    public static final Block ERROR_BLOCK = registerBlock("error_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.DIRT).mapColor(MapColor.PURPLE)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(SpecterCraft.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(SpecterCraft.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerModBlocks() {
+        SpecterCraft.LOGGER.info("registering blocks: " + SpecterCraft.MOD_ID);
+    }
+}
